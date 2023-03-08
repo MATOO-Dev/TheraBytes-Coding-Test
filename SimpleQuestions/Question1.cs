de5787af956df2da71b4248711e4ef5bc64b9a9e
Felix Gold <fagoldob@gmail.com>
2023-03-08 21:20:50 +0100

    Initial repo setup

---
+++ SimpleQuestions/Question1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleQuestions
+{
+    internal class Question1
+    {
+    }
+}
