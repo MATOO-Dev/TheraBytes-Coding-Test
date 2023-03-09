ce9deaf245085cca05fbfbfdfae913704f95c204
MATOO-Dev <fagoldob@gmail.com>
2023-03-09 13:10:51 +0100

    Implemented basic skill tree

--- SkillTree/Program.cs
+++ SkillTree/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+using SkillTree;
+
+//alternatively, move this to a file instead and parse from that
+//create skills for mage class
+SkillNode Mage = new SkillNode("Mage", null);
+SkillNode Fireball = new SkillNode("Fireball", new List<SkillNode> {Mage});
+SkillNode ElectroShock = new SkillNode("ElectroShock", new List<SkillNode> {Fireball});
+SkillNode Freeze = new SkillNode("Freeze", new List<SkillNode> {Fireball});
+SkillNode Thunderbolt = new SkillNode("Thunderbolt", new List<SkillNode> {ElectroShock});
+SkillNode Snowstorm = new SkillNode("Snowstorm", new List<SkillNode> {Freeze});
+
+//create skills for warriror class
+SkillNode Warrior = new SkillNode("Warrior", null);
+SkillNode Strike = new SkillNode("Strike", new List<SkillNode> {Warrior});
+SkillNode Hit = new SkillNode("Hit", new List<SkillNode> {Warrior});
+SkillNode DoubleStrike = new SkillNode("Double Strike", new List<SkillNode> {Strike});
+SkillNode Slash = new SkillNode("Slash", new List<SkillNode> {Strike});
+SkillNode Knockout = new SkillNode("Knockout", new List<SkillNode> {Hit});
+SkillNode RoundhouseKick = new SkillNode("Roundhouse Kick", new List<SkillNode> {Slash, Knockout});
+
+//testing functionality
+Mage.ListData();
+Fireball.ListData();
+ElectroShock.ListData();
+Freeze.ListData();
+Thunderbolt.ListData();
+Snowstorm.ListData();
+Warrior.ListData();
+Strike.ListData();
+Hit.ListData();
+DoubleStrike.ListData();
+Slash.ListData();
+Knockout.ListData();
+RoundhouseKick.ListData();
+
+//all of these should probably be saved in a list instead, e.g. MageSkills and WarriorSkills respectively
+//this would require caching the index of the dependency nodes when creating them, so they can be reused later
---
+++ SkillTree/SkillNode.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+
+namespace SkillTree
+{
+    internal class SkillNode
+    {
+        //name of the skill node
+        public string Name {get; private set;}
+        //whether the skill node is currently locked or unlocked
+        public bool IsLocked {get; private set;}
+        //list of dependency nodes
+        private List<SkillNode> NodeDependencies;
+
+        //constructor
+        public SkillNode (string nodeName, List<SkillNode> dependencies)
+        {
+            Name = nodeName;
+            NodeDependencies = dependencies;
+            IsLocked = true;
+        }
+
+        //check whether the node can be unlocked
+        public bool CanBeUnlocked()
+        {
+            //first, check if parents are available. if no parents are available, the node can be unlocked
+            if(NodeDependencies == null || NodeDependencies.Count == 0)
+                return true;
+            //if parents are available, check if they are already unlocked. if any parent is still locked, return false
+            foreach (SkillNode current in NodeDependencies)
+            {
+                if(current.IsLocked)
+                    return false;
+            }
+            //if all previous checks were passed, return true
+            return true;
+        }
+
+        public void UpdateName(string newName)
+        {
+            Name = newName;
+        }
+
+        public void UpdateDependencies(List<SkillNode> newDependencies)
+        {
+            NodeDependencies = newDependencies;
+        }
+
+        public void ListData()
+        {
+            Console.Write("Name: " + Name + "\n");
+            Console.Write("Currently locked: " + IsLocked + "\n");
+            if(IsLocked)
+            {
+                Console.Write("Node depends on: ");
+                if(NodeDependencies != null)
+                    foreach(SkillNode current in NodeDependencies)
+                    {
+                        Console.Write(current.Name);
+                    }
+                Console.Write("\n");
+                Console.Write("Available for unlock: " + CanBeUnlocked().ToString() + "\n");
+            }
+        }
+    }
+}
